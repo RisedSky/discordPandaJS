a43eeaad97d0197341cc4d54e9a4806689d85025
RisedSky <risedsky@gmail.com>
2018-02-16 22:26:32 +0100

    Updated the server list

--- whitelistServer.js
+++ whitelistServer.js
@@ -1,15 +1,11 @@
-// 412262889156771842 = Official Server
-// 356891225715900427 = Rised-bot Server
-// 368837055993741323 = Benpoke Server
-// 410876225754759169 = ZenDev - Test Bot
-// 378217520236068864  = Sloper39 Server
 var WhiteListServer = [];
 WhiteListServer.push(412262889156771842); //Official Server)
 WhiteListServer.push(356891225715900427); //Rised-bot Server
 WhiteListServer.push(368837055993741323); //Benpoke Server
-WhiteListServer.push(410876225754759169); //ZenDev - Test Bot
 WhiteListServer.push(378217520236068864); //Sloper39 Server
+WhiteListServer.push(412287827787907072); //Downell FR Server
 WhiteListServer.push(368461834929307648); //Laeti Zep Server
 WhiteListServer.push(373919412325056513); //Skelever & Ramzer Server
+WhiteListServer.push(413838785835696129); //Grogeek Server
 
 exports.WhiteListServer = WhiteListServer;
