8cb718036e02a1859019110d43807aedab815968
RisedSky <risedsky@gmail.com>
2018-02-24 02:07:33 +0100

    Added a new file: blacklistuser

---
+++ blacklistUser.js
@@ -0,0 +1,7 @@
+var BlackListUser = [];
+BlackListUser.push(246362462545379328); //Shoox
+BlackListUser.push(403645146245365771); //fathmir lasquale
+BlackListUser.push(238621376238452736); //Astro
+BlackListUser.push(234385324883574786); //Daoudbale
+BlackListUser.push(288028092826648577); //NiNj425
+exports.BlackListUser = BlackListUser;
